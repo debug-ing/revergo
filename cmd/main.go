d52556a6c9d87f961e765428b34c189a8ad571c0
debug-ing <mahdi.bl4ck@gmail.com>
2024-12-21 23:51:19 +0330

    Add config file

--- cmd/main.go
+++ cmd/main.go
@@ -1,10 +1,15 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/debug-ing/revergo/config"
+)
 
 func main() {
 	fmt.Println("Starting the application...")
 	// Load the configuration
-
+	config := config.LoadConfig()
+	fmt.Println("Configuration loaded:", len(config.Projects))
 	//
 }
--- config/config.go
+++ config/config.go
@@ -1,1 +1,32 @@
 package config
+
+import (
+	"log"
+	"sync"
+
+	"github.com/spf13/viper"
+)
+
+type ProjectConfig struct {
+	Name string `mapstructure:"name"`
+}
+
+type AppConfig struct {
+	Projects []ProjectConfig `mapstructure:"projects"`
+}
+
+var once sync.Once
+
+func LoadConfig() (config *AppConfig) {
+	once.Do(func() {
+		viper.SetConfigFile("config/config.toml")
+		if err := viper.ReadInConfig(); err != nil {
+			panic(err)
+		}
+		if err := viper.Unmarshal(&config); err != nil {
+			panic("ERROR load config file!")
+		}
+		log.Println("================ Loaded Configuration ================")
+	})
+	return
+}

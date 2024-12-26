b31b6c18a00e4b1ae4fc0ac1d61311cab1b7cf0a
debug-ing <mahdi.bl4ck@gmail.com>
2024-12-27 01:48:59 +0330

    Fix handleConnection

--- internal/reverse.go
+++ internal/reverse.go
@@ -4,7 +4,6 @@ import (
 	"bufio"
 	"bytes"
 	"fmt"
-	"io"
 	"log"
 	"net"
 	"net/http"
@@ -43,27 +42,38 @@ func (r *Reverse) Reverse() {
 			//
 			// go handleTCPConnection(clientConn, project.Proxy)
 			// go handleTCPConnection2(clientConn, project.Proxy)
-			go handleTCPConnection(clientConn, project.Proxy)
+			go handleConnection(clientConn, project.Proxy)
 		}
 	}
 }
-
-// this function just forward the request to the proxy server
-func handleTCPConnectionWithIoCopy(clientConn net.Conn, proxy string) {
+func handleConnection(clientConn net.Conn, port string) {
 	defer clientConn.Close()
-	targetConn, err := net.Dial("tcp", proxy)
+	backendConn, err := net.Dial("tcp", port)
 	if err != nil {
-		log.Printf("Failed to connect to target server: %v", err)
 		return
 	}
-	defer targetConn.Close()
-	go io.Copy(targetConn, clientConn)
-	io.Copy(clientConn, targetConn)
-}
+	defer backendConn.Close()
+	clientReader := bufio.NewReader(clientConn)
+	backendWriter := bufio.NewWriter(backendConn)
+	for {
+		line, err := clientReader.ReadString('\n')
+		if err != nil {
+			return
+		}
+		if line == "\r\n" {
+			backendWriter.WriteString(line)
+			backendWriter.Flush()
+			break
+		}
+		if strings.HasPrefix(line, "Host:") {
+			line = fmt.Sprintf("Host: %s\r\n", port)
+		}
 
-// this function parse the request and response
-func handleTCPConnection2(clientConn net.Conn, targetAddr string) {
-	defer clientConn.Close()
+		backendWriter.WriteString(line)
+		backendWriter.Flush()
+	}
+	// go io.Copy(backendConn, clientConn)
+	// io.Copy(clientConn, backendConn)
 	buffer := make([]byte, 4096)
 	n, err := clientConn.Read(buffer)
 	if err != nil {
@@ -78,19 +88,13 @@ func handleTCPConnection2(clientConn net.Conn, targetAddr string) {
 		log.Printf("Failed to parse HTTP request: %v", err)
 		return
 	}
-	targetConn, err := net.Dial("tcp", targetAddr)
-	if err != nil {
-		log.Printf("Failed to connect to target server: %v", err)
-		return
-	}
-	defer targetConn.Close()
-	err = httpRequest.Write(targetConn)
+	err = httpRequest.Write(backendConn)
 	if err != nil {
 		log.Printf("Failed to forward request to target server: %v", err)
 		return
 	}
 	responseBuffer := make([]byte, 4096)
-	n, err = targetConn.Read(responseBuffer)
+	n, err = backendConn.Read(responseBuffer)
 	if err != nil {
 		log.Printf("Failed to read response from target server: %v", err)
 		return
@@ -101,61 +105,114 @@ func handleTCPConnection2(clientConn net.Conn, targetAddr string) {
 	clientConn.Write(responseBuffer[:n])
 }
 
-func handleTCPConnection(clientConn net.Conn, targetAddr string) {
-	defer clientConn.Close()
+// this function just forward the request to the proxy server
+// func handleTCPConnectionWithIoCopy(clientConn net.Conn, proxy string) {
+// 	defer clientConn.Close()
+// 	targetConn, err := net.Dial("tcp", proxy)
+// 	if err != nil {
+// 		log.Printf("Failed to connect to target server: %v", err)
+// 		return
+// 	}
+// 	defer targetConn.Close()
+// 	go io.Copy(targetConn, clientConn)
+// 	io.Copy(clientConn, targetConn)
+// }
 
-	// Read client request
-	buffer := make([]byte, 4096)
-	n, err := clientConn.Read(buffer)
-	if err != nil {
-		log.Printf("Error reading client data: %v", err)
-		return
-	}
+// // this function parse the request and response
+// func handleTCPConnection2(clientConn net.Conn, targetAddr string) {
+// 	defer clientConn.Close()
+// 	buffer := make([]byte, 4096)
+// 	n, err := clientConn.Read(buffer)
+// 	if err != nil {
+// 		log.Printf("Error reading client data: %v", err)
+// 		return
+// 	}
+// 	requestLine := string(buffer[:n])
+// 	method, url, _ := parseHTTPRequestLine(requestLine)
+// 	fmt.Printf("HTTP Method: %s, URL: %s\n", method, url)
+// 	httpRequest, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(buffer[:n])))
+// 	if err != nil {
+// 		log.Printf("Failed to parse HTTP request: %v", err)
+// 		return
+// 	}
+// 	targetConn, err := net.Dial("tcp", targetAddr)
+// 	if err != nil {
+// 		log.Printf("Failed to connect to target server: %v", err)
+// 		return
+// 	}
+// 	defer targetConn.Close()
+// 	err = httpRequest.Write(targetConn)
+// 	if err != nil {
+// 		log.Printf("Failed to forward request to target server: %v", err)
+// 		return
+// 	}
+// 	responseBuffer := make([]byte, 4096)
+// 	n, err = targetConn.Read(responseBuffer)
+// 	if err != nil {
+// 		log.Printf("Failed to read response from target server: %v", err)
+// 		return
+// 	}
+// 	statusLine := string(responseBuffer[:n])
+// 	statusCode := extractHTTPStatusCode(statusLine)
+// 	fmt.Printf("Response Status Code: %d\n", statusCode)
+// 	clientConn.Write(responseBuffer[:n])
+// }
 
-	// Parse HTTP request
-	httpRequest, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(buffer[:n])))
-	if err != nil {
-		log.Printf("Failed to parse HTTP request: %v", err)
-		return
-	}
+// func handleTCPConnection(clientConn net.Conn, targetAddr string) {
+// 	defer clientConn.Close()
 
-	// Set the target address as the Host
-	httpRequest.Host = targetAddr
-	httpRequest.RequestURI = "" // Clear the RequestURI for forwarding
+// 	// Read client request
+// 	buffer := make([]byte, 4096)
+// 	n, err := clientConn.Read(buffer)
+// 	if err != nil {
+// 		log.Printf("Error reading client data: %v", err)
+// 		return
+// 	}
 
-	// Connect to the target server
-	targetConn, err := net.Dial("tcp", targetAddr)
-	if err != nil {
-		log.Printf("Failed to connect to target server: %v", err)
-		return
-	}
-	defer targetConn.Close()
+// 	// Parse HTTP request
+// 	httpRequest, err := http.ReadRequest(bufio.NewReader(bytes.NewReader(buffer[:n])))
+// 	if err != nil {
+// 		log.Printf("Failed to parse HTTP request: %v", err)
+// 		return
+// 	}
 
-	// Forward the HTTP request to the target server
-	err = httpRequest.Write(targetConn)
-	if err != nil {
-		log.Printf("Failed to forward request to target server: %v", err)
-		return
-	}
+// 	// Set the target address as the Host
+// 	httpRequest.Host = targetAddr
+// 	httpRequest.RequestURI = "" // Clear the RequestURI for forwarding
 
-	// Read the response from the target server
-	targetReader := bufio.NewReader(targetConn)
-	httpResponse, err := http.ReadResponse(targetReader, httpRequest)
-	if err != nil {
-		log.Printf("Failed to read response from target server: %v", err)
-		return
-	}
-	defer httpResponse.Body.Close()
+// 	// Connect to the target server
+// 	targetConn, err := net.Dial("tcp", targetAddr)
+// 	if err != nil {
+// 		log.Printf("Failed to connect to target server: %v", err)
+// 		return
+// 	}
+// 	defer targetConn.Close()
 
-	// Write the response back to the client
-	err = httpResponse.Write(clientConn)
-	if err != nil {
-		log.Printf("Failed to forward response to client: %v", err)
-		return
-	}
+// 	// Forward the HTTP request to the target server
+// 	err = httpRequest.Write(targetConn)
+// 	if err != nil {
+// 		log.Printf("Failed to forward request to target server: %v", err)
+// 		return
+// 	}
 
-	fmt.Printf("Forwarded request to %s, response status: %d\n", targetAddr, httpResponse.StatusCode)
-}
+// 	// Read the response from the target server
+// 	targetReader := bufio.NewReader(targetConn)
+// 	httpResponse, err := http.ReadResponse(targetReader, httpRequest)
+// 	if err != nil {
+// 		log.Printf("Failed to read response from target server: %v", err)
+// 		return
+// 	}
+// 	defer httpResponse.Body.Close()
+
+// 	// Write the response back to the client
+// 	err = httpResponse.Write(clientConn)
+// 	if err != nil {
+// 		log.Printf("Failed to forward response to client: %v", err)
+// 		return
+// 	}
+
+// 	fmt.Printf("Forwarded request to %s, response status: %d\n", targetAddr, httpResponse.StatusCode)
+// }
 
 func parseHTTPRequestLine(requestLine string) (method, url, version string) {
 	parts := strings.Fields(requestLine)
